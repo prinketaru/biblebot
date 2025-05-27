7013eccbd989b2162d673a8fcb397d608d566116
prinke <93015740+prinketaru@users.noreply.github.com>
2025-05-27 15:31:37 +0000

    update embed

--- helpers/verse_embed.js
+++ helpers/verse_embed.js
@@ -2,6 +2,7 @@ import { EmbedBuilder } from "discord.js";
 
 export default async function verseEmbed(verse, reference, translation) {
     let longtl = "";
+    let iconURL = "";
 
     if (translation === "KJV")
         longtl = "King James Version";
@@ -9,6 +10,8 @@ export default async function verseEmbed(verse, reference, translation) {
         longtl = "New International Version";
     else
         longtl = "English Standard Version";
+        iconURL = "https://i.ibb.co/7xTStG05/id-B5-Dj4-ZHm-1748005133872.png";
+    
 
     const embed = new EmbedBuilder()
         .setDescription(verse)
@@ -17,15 +20,15 @@ export default async function verseEmbed(verse, reference, translation) {
         .setURL(`https://www.biblegateway.com/passage/?search=${encodeURIComponent(reference)}&version=${translation}`)
         .setAuthor({
             name: longtl,
-            iconURL: "https://i.ibb.co/7xTStG05/id-B5-Dj4-ZHm-1748005133872.png",
+            iconURL: iconURL,
         })
         .setTimestamp();
 
     // add a random chance to include a donation message
     if (Math.random() < 0.1) {
-        embed.addFields({
-            name: "Support Me",
-            value: "I'm building BibleBot solo—if it's blessed you, [buy me a coffee](https://buymeacoffee.com/prinke) and help keep it going. 🙏",
+        embed.addFields({   
+            name: "\u200B",
+            value: "-# I'm building BibleBot solo—if it's blessed you, [buy me a coffee](https://buymeacoffee.com/prinke) and help keep it going.",
         })
     }
 
